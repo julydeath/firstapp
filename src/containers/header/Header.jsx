be9357db5412d2220f858ac483e4d34d56fe2d63
julydeath <manojkarajada.mk@gmail.com>
2023-02-02 22:55:51 +0530

    navbar done

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./header.css";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+
 const Header = () => {
-  return <div>This is header</div>;
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          My First UI/UX Project With React and Css.Thanks to
+          <a
+            className="akhil__text"
+            href="https://in.linkedin.com/in/kaparapuakhilnaidu"
+          >
+            Akhil Naidu
+          </a>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
